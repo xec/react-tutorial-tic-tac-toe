38c2a171379080cb410aaa70d2043bced238f29e
Eric Morton <ericbm@yahoo.com>
2019-11-23 23:21:59 +0100

    intro step 2

--- src/std-format-test.js
+++ src/std-format-test.js
@@ -1,6 +1,10 @@
-const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
-[1, 2, 3].forEach(async (num) => {
-  await waitFor(50);
-  console.log(num);
-});
-console.log('Done');
+(function ($, ko, PORTAL) {
+  const waitFor = ms => new Promise(resolve => setTimeout(resolve, ms))
+  const list = [1, 2, 3]
+  list.forEach(async num => {
+    await waitFor(50)
+    console.log(num)
+  })
+
+  console.log('Done')
+})(window.jQuery, window.ko, window.PORTAL = window.PORTAL || {})
