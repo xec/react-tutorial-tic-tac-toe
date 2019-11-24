4c015d8b3c1ed92b6aeaffe9917114515f212921
Eric Morton <ericbm@yahoo.com>
2019-11-24 10:12:33 +0100

    slight formatting change

--- src/index.jsx
+++ src/index.jsx
@@ -21,9 +21,7 @@ class Board extends React.Component {
 
   handleClick (i) {
     const squares = this.state.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
+    if (calculateWinner(squares) || squares[i]) return
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
